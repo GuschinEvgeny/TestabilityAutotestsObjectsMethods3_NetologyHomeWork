1aff14056e64b409db2d69bba5769973ae70cedf
Guschin Evgeny <eg@vidis.tech>
2023-09-18 18:03:07 +0300

    add CaMeL case

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,17 +1,16 @@
 public class CreditPaymentService {
-    public int LoanCalculate (
-            int sum_loan ,
-            double interest ,
-            int loan_time )
-    {
-        int credit_time_month = loan_time * 12 ; // add loan time by month
-        double interest_mont = (interest / 100) / 12 ; // add interest by month
-        double a = Math.pow ((1+interest_mont) , credit_time_month) ; // service action
-        double b = interest_mont * a ; // service action
-        double c = a - 1 ; // service action
-        double result = sum_loan * ( b / c) ; // result action
-        int monthly_pay = (int) result ; // type mapping
+    public int LoanCalculate(
+            int sumLoan,
+            double interest,
+            int loanTime) {
+        int creditTimeMonth = loanTime * 12; // add loan time by month
+        double interestMonth = (interest / 100) / 12; // add interest by month
+        double aPrecalculation = Math.pow((1 + interestMonth), creditTimeMonth); // service action
+        double bPrecalculation = interestMonth * aPrecalculation; // service action
+        double cPrecalculation = aPrecalculation - 1; // service action
+        double result = sumLoan * (bPrecalculation / cPrecalculation); // result action
+        int monthlyPay = (int) result; // type mapping
 
-        return monthly_pay ;
+        return monthlyPay;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        int sum_loan = 1_000_000 ;
-        double interest = 9.99 ; // add percents by year
+        int sumLoan = 1_000_000;
+        double interest = 9.99; // add percents by year
 
-        CreditPaymentService service = new CreditPaymentService ();
-        int payment = service.LoanCalculate( sum_loan , interest , 1);
-        System.out.println("Сумма кредита " + sum_loan + " срок кредита "
-                  +" 1 год");
+        CreditPaymentService service = new CreditPaymentService();
+        int payment = service.LoanCalculate(sumLoan, interest, 1);
+        System.out.println("Сумма кредита " + sumLoan + " срок кредита "
+                + " 1 год");
         System.out.println("Ваш ежемесячный платеж");
-        System.out.println ( "составит : " + payment  );
+        System.out.println("составит : " + payment);
         System.out.println("=============================================");
 
-        CreditPaymentService service2 = new CreditPaymentService ();
-        int payment2 = service2.LoanCalculate( sum_loan , interest , 2);
-        System.out.println("Сумма кредита " + sum_loan + " срок кредита "
-                 + "2 года" ) ;
+        CreditPaymentService service2 = new CreditPaymentService();
+        int payment2 = service2.LoanCalculate(sumLoan, interest, 2);
+        System.out.println("Сумма кредита " + sumLoan + " срок кредита "
+                + "2 года");
         System.out.println("Ваш ежемесячный платеж");
-        System.out.println ( "составит : " + payment2 );
+        System.out.println("составит : " + payment2);
         System.out.println("=============================================");
 
-        CreditPaymentService service3 = new CreditPaymentService ();
-        int payment3 = service3.LoanCalculate( sum_loan , interest , 3);
-        System.out.println("Сумма кредита " + sum_loan + " срок кредита "
-                + "3 года" ) ;
+        CreditPaymentService service3 = new CreditPaymentService();
+        int payment3 = service3.LoanCalculate(sumLoan, interest, 3);
+        System.out.println("Сумма кредита " + sumLoan + " срок кредита "
+                + "3 года");
         System.out.println("Ваш ежемесячный платеж");
-        System.out.println ( "составит : " + payment3 );
+        System.out.println("составит : " + payment3);
         System.out.println("=============================================");
 
     }
